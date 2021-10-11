670521273282f72eed001d33cd7b0f9fe1620c7e
Joe Hertig <joehertig94@gmail.com>
2021-10-10 20:25:39 -0600

    Added Heroku, added more to server.js

--- server.js
+++ server.js
@@ -1,20 +1,50 @@
 const express = require("express");
-
 const PORT = process.env.PORT || 3001;
 const app = express();
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
 
-// Parse incoming string or array data
-app.use(express.urlencoded({ extended: true }));
+const { notes } = require("./Develop/db/db.json");
 
-// parse incoming JSON data
-app.use(express.json());
-app.use(express.static("public"));
+function filterByQuery(query, notesArray) {
+  let filteredResults = notesArray;
+  if (query.title) {
+    filteredResults = filteredResults.filter(
+      (note) => note.title === query.title
+    );
+  }
+  if (query.text) {
+    filteredResults = filteredResults.filter(
+      (note) => note.text === query.text
+    );
+  }
 
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
+  return filteredResults;
+}
+
+function findById(id, notesArray) {
+  const result = notesArray.filter((note) => note.id === id)[0];
+  return result;
+}
+
+app.get("/api/notes", (req, res) => {
+  let results = notes;
+  console.log(req.query);
+  res.json(notes);
+});
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
+
+// const app = express();
+// const apiRoutes = require("./Develop/routes/apiRoutes");
+// const htmlRoutes = require("./Develop/routes/htmlRoutes");
+
+// // Parse incoming string or array data
+// app.use(express.urlencoded({ extended: true }));
+
+// // parse incoming JSON data
+// app.use(express.json());
+// app.use(express.static("public"));
+
+// app.use("/api", apiRoutes);
+// app.use("/", htmlRoutes);
